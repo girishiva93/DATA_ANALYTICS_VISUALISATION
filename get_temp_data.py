b4e6a6fb3e4f276bc9741106f42ef6e3f39dbc70
Shiva Giri <girishiva93@gmail.com>
2025-04-20 19:57:45 +1000

    Fe - Group projects

---
+++ get_temp_data.py
@@ -0,0 +1,7 @@
+# https://www.kaggle.com/datasets/jakewright/2m-daily-weather-history-uk?select=all_weather_data.csv
+
+import kagglehub
+
+path = kagglehub.dataset_download("jakewright/2m-daily-weather-history-uk")
+
+print("Path to dataset files:", path)
